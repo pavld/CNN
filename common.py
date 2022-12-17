7b58f3b91f9f628395d769b9a290e87f94629fc3
pavld <pavlov2001.rt@yandex.ru>
2022-12-17 13:28:05 +0300

    error correction

--- common.py
+++ common.py
@@ -1,8 +1,9 @@
 #необходимые переменные
 class Common ():
-    def __init__(self, fd = 80000, fs = 21000, ti = 0.1, Tc = 2, d = 0.02):
+    def __init__(self, fd = 80000, fs = 21000, ti = 0.1, Tc = 2, d = 0.02, c=1500):
         self.fd = fd
         self.fs = fs
         self.ti = ti
         self.Tc = Tc
         self.d = d
+        self.c = c
